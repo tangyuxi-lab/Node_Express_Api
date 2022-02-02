89df26ad06fd56017ae7f274744e16f3b96d0732
yuxitang <tangyuxi24@gmail.com>
2022-02-02 16:42:53 -0500

    Thorough

--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,7 @@ import express from "express";
 import {createUser, getUser,findUser, deleteUser,updateUser} from "../controllers/users.js";
 const router = express.Router();
 
-let users = [
-    {
-        firstName : "John",
-        lastName : "Doe",
-        age : 25
-    },
-    {
-        firstName : "yuxi",
-        lastName : "Tang",
-        age: 24
-    }
-]
+
 
 router.get('/', getUser);
 
